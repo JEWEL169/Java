618c6b5d93fdb2143b0123c8532587c4ba1c993c
JEWEL169 <jewel.joshy@msam.christuniversity.in>
2024-11-12 19:58:25 +0530

    Create Robber.java
    This Robber class is an abstract base class in Java that defines the structure for classes that inherit from it. It includes:
    
    Concrete Methods:
    
    RobbingClass(): Prints "MScAI&ML".
    MachineLearning(): Prints "I love MachineLearning."
    Abstract Methods:
    
    RowHouses(int[] houses), RoundHouses(int[] houses), SquareHouse(int[] houses), and MuliHouseBuilding(int[] houses): These methods are abstract, meaning they must be implemented by any subclass that extends Robber. Each represents a specific variation of a "House Robber" problem to be solved.

---
+++ Robber.java
@@ -0,0 +1,14 @@
+abstract class Robber {
+    public void RobbingClass() {
+        System.out.println("MScAI&ML");
+    }
+
+    public void MachineLearning() {
+        System.out.println("I love MachineLearning.");
+    }
+
+    public abstract int RowHouses(int[] houses);
+    public abstract int RoundHouses(int[] houses);
+    public abstract int SquareHouse(int[] houses);
+    public abstract int MuliHouseBuilding(int[] houses);
+}
